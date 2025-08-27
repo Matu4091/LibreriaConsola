6d16e04e90c36dfa5e80b4e0f64f079b20e7e6a9
Matu <matu4091@gmail.com>
2025-08-27 13:33:51 -0300

    Primeras Modificaciones

---
+++ QueryBD.sql
@@ -0,0 +1,97 @@
+CREATE DATABASE PRACTICA_01_2025;
+GO
+
+USE PRACTICA_01_2025;
+GO
+
+CREATE TABLE Libros(
+isbn varchar(50),
+titulo varchar(100) not null,
+autor varchar(100) not null,
+nro_paginas int not null,
+stock int not null
+CONSTRAINT PK_Libros PRIMARY KEY (isbn));
+
+
+CREATE TABLE Formas_Pagos(
+id_forma_pago int IDENTITY(1,1),
+forma_pago varchar(100) not null
+CONSTRAINT PK_Formas_Pagos PRIMARY KEY (id_forma_pago));
+
+CREATE TABLE Facturas(
+nro_factura int IDENTITY(1,1),
+fecha date not null,
+id_forma_pago int not null,
+cliente varchar(100)
+CONSTRAINT PK_Facturas PRIMARY KEY (nro_factura),
+CONSTRAINT FK_Facturas_Formas_Pagos FOREIGN KEY (id_forma_pago)
+REFERENCES Formas_Pagos (id_forma_pago));
+
+CREATE TABLE Detalles_Facturas(
+id_detalle_factura int IDENTITY(1,1),
+isbn varchar(50) not null,
+cantidad int not null,
+nro_factura int not null
+CONSTRAINT PK_Detalles_Facturas PRIMARY KEY (id_detalle_factura),
+CONSTRAINT FK_Detalles_Facturas_Libros FOREIGN KEY (isbn)
+REFERENCES Libros (isbn),
+CONSTRAINT FK_Detalles_Facturas_Facturas FOREIGN KEY (nro_factura)
+REFERENCES Facturas (nro_factura));
+
+INSERT INTO Formas_Pagos (forma_pago)
+VALUES ('Debito'),
+       ('Credito'),
+       ('Efectivo'),
+       ('Transferencia');
+
+CREATE PROCEDURE OBTENER_LIBROS
+AS
+BEGIN
+    SELECT * FROM Libros
+END
+
+CREATE PROCEDURE OBTENER_LIBRO_X_ISBN
+@isbn varchar(50)
+AS
+BEGIN
+    SELECT * FROM Libros
+	WHERE isbn LIKE '%@isbn%'
+END
+
+CREATE PROCEDURE MODIFICAR_LIBROS
+@isbn varchar(50),
+@titulo varchar(100),
+@autor varchar(100),
+@nro_paginas int,
+@stock int
+AS 
+BEGIN 
+	IF NOT EXISTS (SELECT 1 FROM Libros WHERE @isbn = isbn)
+	BEGIN
+	     INSERT INTO Libros (isbn, titulo, autor, nro_paginas, stock)
+	     VALUES (@isbn, @titulo, @autor, @nro_paginas, @stock)
+	END
+	ELSE
+	BEGIN	    
+        UPDATE Libros
+	    SET titulo = @titulo, autor = @autor, nro_paginas = @nro_paginas, stock = @stock
+	    WHERE isbn = @isbn
+    END
+END
+
+CREATE PROCEDURE ELIMINAR_LIBRO
+@isbn varchar(50)
+AS
+BEGIN
+    DELETE Libros
+	WHERE isbn = @isbn
+END	 
+
+CREATE PROCEDURE OBTENER_FACTURAS
+
+CREATE PROCEDURE OBTENER_FACTURA_X_ID
+
+CREATE PROCEDURE MODIFICAR_FACTURAS
+AS
+BEGIN
+    INSERT INTO
